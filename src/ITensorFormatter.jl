f6fc03af98d071bc865aa75682c8bc143ceae182
mtfishman <mfishman@flatironinstitute.org>
2026-02-11 22:08:48 -0500

    More fixes

--- src/ITensorFormatter.jl
+++ src/ITensorFormatter.jl
@@ -38,12 +38,17 @@ function organize_import_block(input)
     siblings = []
 
     child_nodes = children(x)
-    ind = findfirst(is_using_or_import, child_nodes)
+    first_ind = findfirst(is_using_or_import, child_nodes)
 
-    while !isnothing(ind)
-        push!(siblings, popat!(child_nodes, ind))
-        ind = findfirst(is_using_or_import, child_nodes)
+    for ind in first_ind:length(child_nodes)
+        if is_using_or_import(child_nodes[ind])
+            push!(siblings, child_nodes[ind])
+        else
+            break
+        end
     end
+
+    src = JuliaSyntax.sourcetext(input)
 
     # Collect all modules and symbols
     using_mods = Set{String}()
@@ -51,35 +56,24 @@ function organize_import_block(input)
     import_mods = Set{String}()
     import_syms = Dict{String, Set{String}}()
 
-    # Joins e.g. [".", ".", "Foo", "Bar"] (from "using ..Foo.Bar") to "..Foo.Bar"
-    function module_join(x)
-        io = IOBuffer()
-        for y in children(x)[1:(end - 1)]
-            print(io, y.val)
-            y.val == "." && continue
-            print(io, ".")
-        end
-        print(io, children(x)[end].val)
-        return String(take!(io))
-    end
+    # Extract the source text of a node, trimming whitespace
+    node_text(x) = strip(src[char_range(x)])
 
     for s in siblings
         isusing = kind(s) === K"using"
         for a in children(s)
             if kind(a) === K":"
                 a_args = children(a)
-                mod = module_join(a_args[1])
+                mod = node_text(a_args[1])
                 set = get!(Set, isusing ? using_syms : import_syms, mod)
                 for i in 2:length(a_args)
-                    push!(set, join(y.val for y in children(a_args[i])))
+                    push!(set, String(node_text(a_args[i])))
                 end
-            elseif kind(a) === K"."
-                push!(isusing ? using_mods : import_mods, module_join(a))
-            elseif kind(a) === K"importpath"
-                push!(isusing ? using_mods : import_mods, String(children(a)[1].val))
+            elseif kind(a) === K"." || kind(a) === K"importpath"
+                push!(isusing ? using_mods : import_mods, String(node_text(a)))
             elseif !isusing && kind(a) === K"as"
                 a_args = children(a)
-                push!(import_mods, join((children(a_args[1])[1].val, "as", a_args[2]), " "))
+                push!(import_mods, node_text(a_args[1]) * " as " * node_text(a_args[end]))
             else
                 error("Unexpected syntax in using/import statement.")
             end
@@ -91,19 +85,29 @@ function organize_import_block(input)
     #       BlueStyle (modules, types, ..., functions) since usually CamelCase is used for
     #       modules, types, etc, but possibly this can be improved by using information
     #       available from SymbolServer
+    # Sort symbols, but keep the module self-reference first if present
+    function sort_with_self_first(syms, self)
+        self′ = pop!(syms, self, nothing)
+        sorted = sort!(collect(syms))
+        if self′ !== nothing
+            pushfirst!(sorted, self)
+        end
+        return sorted
+    end
+
     import_lines = String[]
     for m in import_mods
         push!(import_lines, "import " * m)
     end
     for (m, s) in import_syms
-        push!(import_lines, "import " * m * ": " * join(sort!(collect(s)), ", "))
+        push!(import_lines, "import " * m * ": " * join(sort_with_self_first(s, m), ", "))
     end
     using_lines = String[]
     for m in using_mods
         push!(using_lines, "using " * m)
     end
     for (m, s) in using_syms
-        push!(using_lines, "using " * m * ": " * join(sort!(collect(s)), ", "))
+        push!(using_lines, "using " * m * ": " * join(sort_with_self_first(s, m), ", "))
     end
     io = IOBuffer()
     join(io, sort!(import_lines), "\n")
@@ -113,8 +117,6 @@ function organize_import_block(input)
 
     # Line wrap the using/import statements only
     formatted = JuliaFormatter.format_text(str_to_fmt; join_lines_based_on_source = true)
-
-    src = JuliaSyntax.sourcetext(input)
 
     first_pos = first(char_range(siblings[1]))
     last_pos = last(char_range(siblings[end]))
